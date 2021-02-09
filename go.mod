84bfffbb45809349bb5916ede7be6668b4054837
Tadej <tadej.996@gmail.com>
2021-02-09 18:59:52 +0100

    Add basic image transformation.

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/tdxius/go-image-cdn
 
 go 1.15
 
-require (
-	github.com/disintegration/imaging v1.6.2 // indirect
-	golang.org/x/image v0.0.0-20201208152932-35266b937fa6 // indirect
-)
+require github.com/disintegration/imaging v1.6.2
--- server.go
+++ server.go
@@ -7,32 +7,48 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"github.com/disintegration/imaging"
 )
 
-func homePage(writer http.ResponseWriter, response *http.Request)  {
-	url := "https://upload.wikimedia.org/wikipedia/commons/0/0a/Triglav.jpg"
-	imageResponse, err := http.Get(url)
+func loadImage(url string) image.Image {
+	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil
 	}
 	defer response.Body.Close()
 
-	downloadedImage, _, err := image.Decode(imageResponse.Body)
+	remoteImage, _, err := image.Decode(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
 
+	return remoteImage
+}
+
+func saveImage(srcImage image.Image) {
 	f, err := os.Create("img.jpg")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-	jpeg.Encode(f, downloadedImage, nil)
+	jpeg.Encode(f, srcImage, nil)
+}
 
+func index(writer http.ResponseWriter, response *http.Request)  {
+	url := "https://upload.wikimedia.org/wikipedia/commons/0/0a/Triglav.jpg"
+	srcImage := loadImage(url)
+
+	transformedImage := imaging.Resize(srcImage, 128, 128, imaging.Lanczos)
+
+	saveImage(transformedImage)
 
 	fmt.Fprint(writer, "Homepage endpoint hit 1")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", index)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
