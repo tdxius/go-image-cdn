bf22a8f53bbd4037ad411ee50f813ce2d2c6569c
Tadej <tadej.996@gmail.com>
2021-02-10 10:22:51 +0100

    Handle no image at URL, extract deliverable image, add encoding to default format.

---
+++ deliverable_image.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"github.com/disintegration/imaging"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"net/http"
+	"strings"
+)
+
+type DeliverableImage struct {
+	source image.Image
+	format string
+}
+
+func NewDeliverableImageFromUrl(url string) *DeliverableImage {
+	response, httpError := http.Get(url)
+	if httpError != nil {
+		return nil
+	}
+	defer response.Body.Close()
+
+	contentType := response.Header.Get("Content-Type")
+	if !strings.HasPrefix(contentType, "image/") {
+		return nil
+	}
+
+	remoteImage, _, decodingError := image.Decode(response.Body)
+	if decodingError != nil {
+		return nil
+	}
+
+	return &DeliverableImage{
+		source: remoteImage,
+		format: strings.Split(contentType, "/")[1],
+	}
+}
+
+func (image DeliverableImage) scale(width int, height int) DeliverableImage {
+	if width != 0 && height != 0 {
+		image.source = imaging.Fit(image.source, width, height, imaging.Lanczos)
+	}
+
+	if width == 0 && height != 0 {
+		image.source = imaging.Resize(image.source, 0, height, imaging.Lanczos)
+	}
+
+	if height == 0 && width != 0 {
+		image.source = imaging.Resize(image.source, width, 0, imaging.Lanczos)
+	}
+
+	return image
+}
+
+func (image DeliverableImage) encode(format string) *bytes.Buffer {
+	buffer := new(bytes.Buffer)
+
+	switch format {
+	case "jpeg", "jpg":
+		_ = jpeg.Encode(buffer, image.source, nil)
+	case "png":
+		_ = png.Encode(buffer, image.source)
+	case "gif":
+		_ = gif.Encode(buffer, image.source, nil)
+	default:
+		image.encode(image.format)
+	}
+
+	return buffer
+}
--- server.go
+++ server.go
@@ -3,100 +3,54 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"image"
-	"image/jpeg"
-	"image/png"
 	"log"
 	"net/http"
-	"github.com/disintegration/imaging"
 	"strconv"
 )
 
-func loadImage(url string) image.Image {
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	defer response.Body.Close()
-
-	remoteImage, _, err := image.Decode(response.Body)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	return remoteImage
-}
-
-func encodeImage(srcImage image.Image, format string) *bytes.Buffer {
-	buffer := new(bytes.Buffer)
-	var encodingError error
-
-	switch format {
-	case "jpeg":
-		encodingError = jpeg.Encode(buffer, srcImage, nil)
-	case "png":
-		encodingError = png.Encode(buffer, srcImage)
-	default:
-		encodingError = jpeg.Encode(buffer, srcImage, nil)
-	}
-
-	if encodingError != nil {
-		fmt.Println("Unable to encode image: " + encodingError.Error())
-	}
-
-	return buffer
-}
-
-func scaleImage(srcImage image.Image, width int, height int) image.Image {
-	if width != 0 && height != 0 {
-		return imaging.Fit(srcImage, width, height, imaging.Lanczos)
-	}
-
-	if width == 0 && height != 0 {
-		return imaging.Resize(srcImage, 0, height, imaging.Lanczos)
-	}
-
-	if height == 0 && width != 0 {
-		return imaging.Resize(srcImage, width, 0, imaging.Lanczos)
-	}
-
-	return srcImage
-}
-
-func writeImage(writer http.ResponseWriter, buffer *bytes.Buffer) {
+func imageResponse(writer http.ResponseWriter, buffer *bytes.Buffer) {
 	writer.Header().Set("Content-Type", "image/jpeg")
 	writer.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
 
 	_, writeError := writer.Write(buffer.Bytes())
 	if writeError != nil {
-		log.Println("unable to write image.")
+		log.Println("unable to write source.")
 	}
 }
 
-func index(writer http.ResponseWriter, response *http.Request) {
+func getQueryParamAsInt(request *http.Request, key string) int {
+	value, _ := strconv.ParseInt(request.URL.Query().Get(key), 10, 16)
+	return int(value)
+}
+
+func getQueryParams(request *http.Request) (width int, height int, format string) {
+	width = getQueryParamAsInt(request, "width")
+	height = getQueryParamAsInt(request, "height")
+	format = request.URL.Query().Get("format")
+	return
+}
+
+func index(writer http.ResponseWriter, request *http.Request) {
 	baseUrl := "https://enavtika.si"
-	imageUrl := baseUrl + response.URL.Path
+	imageUrl := baseUrl + request.URL.Path
 	fmt.Println(imageUrl)
 
-	srcImage := loadImage(imageUrl)
-	if srcImage == nil {
-		fmt.Fprint(writer, "No image found at URL: "+imageUrl)
+	deliverableImage := NewDeliverableImageFromUrl(imageUrl)
+	if deliverableImage == nil {
+		fmt.Println("No image found at URL: " + imageUrl)
+		writer.WriteHeader(404)
+		return
 	}
 
-	width, _ := strconv.ParseInt(response.URL.Query().Get("width"), 10, 16)
-	height, _ := strconv.ParseInt(response.URL.Query().Get("height"), 10, 16)
-	scaledImage := scaleImage(srcImage, int(width), int(height))
+	width, height, format := getQueryParams(request)
+	bufferedImage := deliverableImage.scale(width, height).encode(format)
 
-	format := response.URL.Query().Get("format")
-	encodedImage := encodeImage(scaledImage, format)
-
-	writeImage(writer, encodedImage)
+	imageResponse(writer, bufferedImage)
 }
 
 func main() {
 	http.HandleFunc("/", index)
+
 	fmt.Println("Web server is listening on port 80")
 	serverError := http.ListenAndServe(":80", nil)
 	if serverError != nil {
